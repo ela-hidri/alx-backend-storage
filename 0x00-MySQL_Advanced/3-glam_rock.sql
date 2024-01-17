2be6e09a3506f9dfe2fbf3e3f2a8df9f5af61066
ela-hidri <elahidri.eh@gmail.com>
2024-01-17 12:24:16 -0800

    fixing 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
---lists all bands with Glam rock as their main style, ranked by their longevity
+-- lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, IF(split IS NULL, 2022 - formed, split - formed) AS 'lifespan'
 FROM `metal_bands`
 WHERE style Like '%Glam rock%'
