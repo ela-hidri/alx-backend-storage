3f4e81ba1e7b47e0f40cfeb590913dbc336e7244
ela-hidri <elahidri.eh@gmail.com>
2024-01-22 13:38:55 -0800

    adding 10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+pymongo
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name
+    """
+    mongo_collection.update_one({"name": name}, {"$set": {'topics': topics}})
