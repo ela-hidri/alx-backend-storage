a0efd534b0d3a57640e5502ff99c2410a702c36d
ela-hidri <elahidri.eh@gmail.com>
2024-01-24 13:08:18 -0800

    adding changes to exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,20 @@ def count_calls(method: Callable) -> Callable:
     
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """
+    store the history of inputs and outputs for a particular function
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args) -> str:
+        """ store the history """
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+        output = method(self, *args)
+        self._redis.rpush(f"{method.__qualname__}:outputs", output)
+        return output
+
+    return wrapper
+
 
 class Cache:
     """
@@ -31,6 +45,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
